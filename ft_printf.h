b4b9e60eb06ce8a8c79da3efb66561f4e0291d9b
Rylan Muller <rymuller@e1r8p2.21-school.ru>
2019-03-01 15:25:03 +0300

    alpha

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rymuller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 10:35:49 by rymuller          #+#    #+#             */
-/*   Updated: 2019/02/27 12:53:08 by rymuller         ###   ########.fr       */
+/*   Updated: 2019/03/01 15:17:56 by rymuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,34 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-typedef struct	s_specifier
+typedef union		u_num_int
 {
-	int		minus:1;
-	int		plus:1;
-	int		null:1;
-	int		space:1;
-	int		sharp:1;
-	int		width;
-	int		dot:1;
-	int		precision;
-	char		size[3];
-	int		num_bytes;
-}			t_specifier;
+	short			num_s;
+	int				num_i;
+	long int		num_l;
+	long long int	num_ll;
+}					t_num_int;
 
-void			ft_putadr(va_list arg, t_specifier *specifier);
-void			ft_putstr(va_list arg, t_specifier *specifier);
-void			ft_putchar(va_list arg, t_specifier *specifier);
-void			ft_putdec_int(va_list arg, t_specifier *specifier);
-char			*ft_itoa_base(size_t num, size_t base, char *buffer);
-char			*specifier_parse(char *format, t_specifier *specifier, va_list arg);
+typedef struct		s_specifier
+{
+	unsigned int	minus:1;
+	unsigned int	plus:1;
+	unsigned int	null:1;
+	unsigned int	space:1;
+	unsigned int	sharp:1;
+	int				width;
+	unsigned int	dot:1;
+	int				precision;
+	char			size[3];
+	t_num_int		num_int;
+	int				num_bytes;
+}					t_specifier;
 
+void				ft_putadr(va_list arg, t_specifier *specifier);
+void				ft_putstr(va_list arg, t_specifier *specifier);
+void				ft_putchar(va_list arg, t_specifier *specifier);
+void				ft_putdec_int(va_list arg, t_specifier *specifier);
+char				*ft_itoa_base(size_t num, size_t base, char *buffer);
+char				*specifier_parse(char *format, t_specifier *specifier,
+		va_list arg);
 #endif
